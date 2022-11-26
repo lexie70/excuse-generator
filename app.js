7cf7e824edf665db838f54841a4d452aee854cba
lexie70 <alexandracampos658@gmail.com>
2022-11-26 20:02:45 +0000

    segundo commit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let who = ["the dog","muy grandma","his turtle","my bird"]; //0-3
 let action = ["ate","peed","crushed","broke"]; //0-3
 let what = ["my homework","the keys","the car"]; //0-2
-let when = ["before the class","right on time","when i finished","durin my luch","while i was praying"]; //0-4
+let when = ["before the class","right on time","when i finished","durin my luch","while i was praying"]; //0-4// Returns a random integer from 0 to 9:
+Math.floor(Math.random() * 10);
